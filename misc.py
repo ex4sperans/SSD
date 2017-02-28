0009bb01990d4e2072311296aaff1d43484f2ecc
d.danevskyi <d.danevskyi@gmail.com>
2017-03-01 00:50:02 +0200

    move several functions to misc

--- misc.py
+++ misc.py
@@ -1,4 +1,9 @@
 import itertools
+import json
+import os
+import fnmatch
+
+from scipy import ndimage
 
 def flatten_list(nested_list):
     #flatten nested list of arbitrary depth
@@ -15,3 +20,21 @@ def height_and_width(shape):
     else:
         raise ValueError('Could not infer height and'\
             ' width from shape {shape}.'.format(shape=shape))
+
+def load_image(image_path):
+        return ndimage.imread(image_path)
+
+def load_json(file_path):
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        return data
+
+def resize(image, new_shape):
+        return misc.imresize(image, new_shape) 
+
+def find_files(path, file_type, sort=True):
+    files = list()
+    for root, dirnames, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, file_type):
+            files.append(os.path.join(root, filename))
+    return sorted(files) if sort else files
--- voc_loader.py
+++ voc_loader.py
@@ -1,29 +1,25 @@
-import json
 import os
 import xml.etree.ElementTree as xml_parser
-import fnmatch
 import random
 import functools
 
 import numpy as np
-import matplotlib
-from matplotlib import pyplot as plt
-from scipy import ndimage
 
 from boxes import BoundBox, CenterBox
+import misc
 
 class VOCLoader:
 
     def __init__(self, dataset_params_path='voc_dataset_params.json', preprocessing=None):
 
-        self.dataset_params = VOCLoader.load_json(dataset_params_path)
-        self.train_images = VOCLoader.find_files(
+        self.dataset_params = misc.load_json(dataset_params_path)
+        self.train_images = misc.find_files(
             self.dataset_params['train']['images'], '*.jpg')
-        self.train_annotations = VOCLoader.find_files(
+        self.train_annotations = misc.find_files(
             self.dataset_params['train']['annotations'], '*.xml')
-        self.test_images = VOCLoader.find_files(
+        self.test_images = misc.find_files(
             self.dataset_params['test']['images'], '*.jpg')
-        self.test_annotations = VOCLoader.find_files(
+        self.test_annotations = misc.find_files(
             self.dataset_params['test']['annotations'], '*.xml')
 
         self.train_set = list(zip(self.train_images, self.train_annotations))
@@ -31,14 +27,6 @@ class VOCLoader:
 
         self._set_preprocessing_fn(preprocessing)
         
-    @staticmethod  
-    def find_files(path, file_type, sort=True):
-        files = list()
-        for root, dirnames, filenames in os.walk(path):
-            for filename in fnmatch.filter(filenames, file_type):
-                files.append(os.path.join(root, filename))
-        return sorted(files) if sort else files
-
     @staticmethod
     def parse_annotation(annotation_path):
         root = xml_parser.parse(annotation_path).getroot()
@@ -54,7 +42,7 @@ class VOCLoader:
     def new_batch(self, batch_size):
 
         batch = random.sample(self.train_set, batch_size)
-        batch = [(VOCLoader.load_image(image), VOCLoader.parse_annotation(annotation))
+        batch = [(misc.load_image(image), VOCLoader.parse_annotation(annotation))
                 for image, annotation in batch]
 
         if hasattr(self, '_preprocess'):
@@ -68,36 +56,6 @@ class VOCLoader:
             preprocessing_type, preprocessing_params = preprocessing
             if preprocessing_type == 'resize':
                 self._preprocess = functools.partial(
-                    VOCLoader.resize, new_shape=preprocessing_params)
+                    misc.resize, new_shape=preprocessing_params)
 
-    @staticmethod
-    def plot_with_bbox(image, annotation, save_path):
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.imshow(image)
-
-        for class_name, bbox in annotation['objects']:
-            xmin, ymin, xmax, ymax = bbox 
-            bx = (xmin, xmax, xmax, xmin, xmin)
-            by = (ymin, ymin, ymax, ymax, ymin)
-            ax.plot(bx, by, c='b', lw=2)
-            
-        ax.set_axis_off()    
-        os.makedirs(save_path, exist_ok=True)
-        fig.savefig(os.path.join(save_path, annotation['file_name']))
-        plt.close()
-
-    @staticmethod
-    def load_image(image_path):
-        return ndimage.imread(image_path)
-
-    @staticmethod
-    def load_json(file_path):
-        with open(file_path, 'r') as f:
-            data = json.load(f)
-        return data
-
-    @staticmethod
-    def resize(image, new_shape):
-        return misc.imresize(image, new_shape) 
+    
