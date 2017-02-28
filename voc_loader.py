4567c933c36cd36bc1eedf2e4e4eba1f7df3c1a5
d.danevskyi <d.danevskyi@gmail.com>
2017-02-28 15:30:51 +0200

    several changes to loader

--- voc_loader.py
+++ voc_loader.py
@@ -10,13 +10,13 @@ import matplotlib
 from matplotlib import pyplot as plt
 from scipy import ndimage
 
-from boxes import BoundBox
+from boxes import BoundBox, CenterBox
 
 class VOCLoader:
 
-    def __init__(self, set_params_path, preprocessing=None):
+    def __init__(self, dataset_params_path='voc_dataset_params.json', preprocessing=None):
 
-        self.dataset_params = VOCLoader.load_json(set_params_path)
+        self.dataset_params = VOCLoader.load_json(dataset_params_path)
         self.train_images = VOCLoader.find_files(
             self.dataset_params['train']['images'], '*.jpg')
         self.train_annotations = VOCLoader.find_files(
@@ -28,24 +28,26 @@ class VOCLoader:
 
         self.train_set = list(zip(self.train_images, self.train_annotations))
         self.test_set = list(zip(self.test_images, self.test_annotations))
+
+        self._set_preprocessing_fn(preprocessing)
         
     @staticmethod  
-    def find_files(self, path, file_type):
+    def find_files(path, file_type, sort=True):
         files = list()
         for root, dirnames, filenames in os.walk(path):
             for filename in fnmatch.filter(filenames, file_type):
                 files.append(os.path.join(root, filename))
-        return files
+        return sorted(files) if sort else files
 
     @staticmethod
-    def parse_annotation(self, annotation_path):
+    def parse_annotation(annotation_path):
         root = xml_parser.parse(annotation_path).getroot()
         annotation = dict()
         annotation['file_name'] = root.find('filename').text
         annotation['objects'] = list()
         for obj in root.findall('object'):
             #xmin, ymin, xmax, ymax
-            bbox = BoundBox(*[coord.text for coord in obj.find('bndbox')])
+            bbox = BoundBox(*[int(coord.text) for coord in obj.find('bndbox')])
             annotation['objects'].append((obj.find('name').text, bbox))
         return annotation
 
@@ -61,7 +63,7 @@ class VOCLoader:
 
         return batch
 
-    def _set_preprocessing_fn(preprocessing):
+    def _set_preprocessing_fn(self, preprocessing):
         if isinstance(preprocessing, tuple):
             preprocessing_type, preprocessing_params = preprocessing
             if preprocessing_type == 'resize':
@@ -69,11 +71,29 @@ class VOCLoader:
                     VOCLoader.resize, new_shape=preprocessing_params)
 
     @staticmethod
-    def load_image(self, image_path):
+    def plot_with_bbox(image, annotation, save_path):
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.imshow(image)
+
+        for class_name, bbox in annotation['objects']:
+            xmin, ymin, xmax, ymax = bbox 
+            bx = (xmin, xmax, xmax, xmin, xmin)
+            by = (ymin, ymin, ymax, ymax, ymin)
+            ax.plot(bx, by, c='b', lw=2)
+            
+        ax.set_axis_off()    
+        os.makedirs(save_path, exist_ok=True)
+        fig.savefig(os.path.join(save_path, annotation['file_name']))
+        plt.close()
+
+    @staticmethod
+    def load_image(image_path):
         return ndimage.imread(image_path)
 
     @staticmethod
-    def load_json(self, file_path):
+    def load_json(file_path):
         with open(file_path, 'r') as f:
             data = json.load(f)
         return data
