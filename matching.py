a45329ab142d8dc33c3f02934ab988d8e677c826
d.danevskyi <d.danevskyi@gmail.com>
2017-03-13 12:01:49 +0200

    update matching

--- matching.py
+++ matching.py
@@ -6,10 +6,10 @@ import misc
 def match_boxes(annotations, image, default_boxes, out_shapes, threshold):
 
     matches = list()
-    top_match = {'overlap': 0, 'match': None}
     image_height, image_width = boxes.height_and_width(image.shape)
     #default boxes is assumed to be a 4d nested list
     for class_name, ground_truth_box in annotations:
+        top_match = {'overlap': 0, 'match': None}
         for i, out_shape in enumerate(out_shapes):
             out_height, out_width = misc.height_and_width(out_shape)
             ground_truth_box = boxes.centerbox_to_boundbox(ground_truth_box)
@@ -20,14 +20,21 @@ def match_boxes(annotations, image, default_boxes, out_shapes, threshold):
             x_max = min(ceil((scaled_ground_truth_box.x_max)*out_width), out_width)
             y_max = min(ceil((scaled_ground_truth_box.y_max)*out_height), out_height)
 
+            matched_boxes = 0
+
             for x in range(x_min, x_max):
                 for y in range(y_min, y_max):
                     for default_box in default_boxes[i][x][y]:
                         overlap = boxes.jaccard_overlap(scaled_ground_truth_box, default_box)
                         if overlap > threshold:
                             matches.append((default_box, (scaled_ground_truth_box, class_name)))
+                            matched_boxes += 1
                         if overlap > top_match['overlap']:
                             top_match['overlap'] = overlap
                             top_match['match'] = (default_box, (scaled_ground_truth_box, class_name))
+            # if ground truth boxes was not matched to any default box
+            # choose top match as only matched box
+            if not matched_boxes:
+                matches.append(top_match['match'])
 
-    return matches if matches else [top_match['match']]
+    return matches
