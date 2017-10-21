b002835526774e7da6855f76cb51524e2146148d
d.danevskyi <d.danevskyi@gmail.com>
2017-10-21 20:11:10 +0300

    Add rescale method for boxes

--- box_arrays.py
+++ box_arrays.py
@@ -20,22 +20,54 @@ class BoundBoxArray(pd.DataFrame):
 
     @classmethod
     def from_boxes(cls, boxes, classnames=None):
+        """Initialize BoundBoxArray from list of bounding boxes.
+
+        Args:
+            boxes: list of tuples: (x_min, y_min, x_max, y_max)
+            classnames: list of classnames for bounding boxes, optional
+        """
         return cls(pd.DataFrame.from_records(boxes,
                                              index=classnames,
                                              columns=BOUNDBOX_COLUMNS))
 
     def as_centerbox_array(self):
+        """Returns correspoding CenterBoxInstance"""
         centerboxes = np.matmul(self.as_matrix(), BOUNDBOX_TO_CENTERBOX)
         return CenterBoxArray.from_boxes(centerboxes, classnames=self.index)
+
+    def rescale(self, scale: tuple):
+        """Rescale accoding to `scale`."""
+
+        vertical, horizontal = scale
+        scale = np.array([horizontal, vertical, horizontal, vertical])
+
+        scaled = self.as_matrix() / scale
+        return BoundBoxArray.from_boxes(scaled, classnames=self.index)
 
 
 class CenterBoxArray(pd.DataFrame):
 
     @classmethod
     def from_boxes(cls, boxes, classnames=None):
+        """Initialize CenterBoxArray from list of bounding boxes.
+
+        Args:
+            boxes: list of tuples: (x_center, y_center, width, height)
+            classnames: list of classnames for center boxes, optional
+        """
         return cls(pd.DataFrame.from_records(boxes,
                                              index=classnames,
                                              columns=CENTERBOX_COLUMNS))
     def as_boundbox_array(self):
+        """Returns correspoding BoundBoxInstance"""
         boundboxes = np.matmul(self.as_matrix(), CENTERBOX_TO_BOUNDBOX)
         return BoundBoxArray.from_boxes(boundboxes, classnames=self.index)
+
+    def rescale(self, scale: tuple):
+        """Rescale accoding to `scale`."""
+
+        vertical, horizontal = scale
+        scale = np.array([horizontal, vertical, horizontal, vertical])
+
+        scaled = self.as_matrix() / scale
+        return CenterBoxArray.from_boxes(scaled, classnames=self.index)
--- tests/test_box_arrays.py
+++ tests/test_box_arrays.py
@@ -4,12 +4,13 @@ import pytest
 
 from box_arrays import CenterBoxArray, BoundBoxArray
 
+
 @pytest.fixture
-def initial_boundboxes():
+def boundboxes():
     return [(20.0, 30.0, 80.0, 100.0), (70.0, 100.0, 100.0, 200.0)]
 
 @pytest.fixture
-def initial_centerboxes():
+def centerboxes():
     return [(50.0, 65.0, 60.0, 70.0), (85.0, 150.0, 30.0, 100.0)]
 
 
@@ -18,41 +19,66 @@ def classnames():
     return ["cat", "dog"]
 
 
-def test_boundbox_array_creation(initial_boundboxes, classnames):
+def test_boundbox_array_creation(boundboxes, classnames):
     """Test functionality of BoundBoxArray creation"""
 
-    boundbox_array = BoundBoxArray.from_boxes(initial_boundboxes, classnames)
+    boundbox_array = BoundBoxArray.from_boxes(boundboxes, classnames)
 
     assert boundbox_array.shape == (2, 4)
     assert (boundbox_array.index == classnames).all()
 
     # without classnames
-    boundbox_array = BoundBoxArray.from_boxes(initial_boundboxes)
+    boundbox_array = BoundBoxArray.from_boxes(boundboxes)
 
     assert boundbox_array.shape == (2, 4)
     assert (boundbox_array.index == (0, 1)).all()
 
 
-def test_center_array_creation(initial_centerboxes, classnames):
+def test_center_array_creation(centerboxes, classnames):
     """Test functionality of CenterBoxArray creation"""
 
-    centerbox_array = CenterBoxArray.from_boxes(initial_centerboxes, classnames)
+    centerbox_array = CenterBoxArray.from_boxes(centerboxes, classnames)
 
     assert centerbox_array.shape == (2, 4)
     assert (centerbox_array.index == classnames).all()
 
     # without classnames
-    centerbox_array = CenterBoxArray.from_boxes(initial_centerboxes)
+    centerbox_array = CenterBoxArray.from_boxes(centerboxes)
 
     assert centerbox_array.shape == (2, 4)
     assert (centerbox_array.index == (0, 1)).all()
 
 
-def test_box_conversion(initial_boundboxes, initial_centerboxes, classnames):
+def test_box_conversion(boundboxes, centerboxes, classnames):
     """Test functionality of conversion between arrays"""
 
-    boundbox_array = BoundBoxArray.from_boxes(initial_boundboxes, classnames)
-    centerbox_array = CenterBoxArray.from_boxes(initial_centerboxes, classnames)
+    boundbox_array = BoundBoxArray.from_boxes(boundboxes, classnames)
+    centerbox_array = CenterBoxArray.from_boxes(centerboxes, classnames)
 
     assert boundbox_array.equals(centerbox_array.as_boundbox_array())
     assert centerbox_array.equals(boundbox_array.as_centerbox_array())
+
+
+def test_box_rescale(boundboxes, centerboxes):
+    """Test functionality of boxes scaling"""
+
+    scale = (3, 4)
+    scales = (4, 3, 4, 3)
+
+    boundbox_array = BoundBoxArray.from_boxes(boundboxes)
+    centerbox_array = CenterBoxArray.from_boxes(centerboxes)
+
+
+    scaled_boundboxes = [tuple(x / s for x, s in zip(box, scales))
+                         for box in boundboxes]
+    scaled_boundbox_array = BoundBoxArray.from_boxes(scaled_boundboxes)
+
+    scaled_centerboxes = [tuple(x / s for x, s in zip(box, scales))
+                          for box in centerboxes]
+    scaled_centerbox_array = CenterBoxArray.from_boxes(scaled_centerboxes)
+
+    assert scaled_boundbox_array.equals(boundbox_array.rescale(scale))
+    assert scaled_centerbox_array.equals(centerbox_array.rescale(scale))
+
+    assert np.allclose(boundbox_array.rescale(scale).as_matrix(),
+        centerbox_array.rescale(scale).as_boundbox_array().as_matrix())
