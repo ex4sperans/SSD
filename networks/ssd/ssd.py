a16bde2b3a5742d0f6ba7ef995df4b4556d3b961
Dmitriy Danevskiy <d.danevskyi@gmail.com>
2018-01-16 00:43:33 +0200

    Fix nested getattr usage

--- networks/ssd/ssd.py
+++ networks/ssd/ssd.py
@@ -349,7 +349,7 @@ class SSD:
 
                 tf.summary.image(
                     layer,
-                    self._nested_getattr(self, layer)[:, :, :, :3],
+                    self._nested_getattr(layer)[:, :, :, :3],
                     max_outputs=1
                 )
 
